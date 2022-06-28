acdab59e9d04b8f48700da3d58ee326d4447ee01
jamil gafur <jamilggafur@gmail.com>
2022-06-28 17:56:43 -0500

    added dataloaders and removed pycatch

--- Model/main.py
+++ Model/main.py
@@ -1,5 +1,6 @@
 from torch.utils.data import random_split as rnd_splt
 from chrom_dataset import Chromatin_Dataset
+from torch.utils.data import DataLoader
 
 
 
@@ -8,6 +9,7 @@ def main():
    # we are doing a 75/25 split
    train_percent = .75
    train_set, val_set = rnd_splt(chrom_data, [int(chrom_data.length*train_percent), chrom_data.length-int(chrom_data.length*train_percent)])
-   print(len(train_set))
-
+   train_loader = DataLoader(train_set, batch_size=32,shuffle=True)
+   test_loader = DataLoader(val_set, batch_size=32, shuffle=True)
+   
 main()
