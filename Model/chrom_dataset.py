69652eab602c3eb9ae50af51fd1219decfc2362a
jamil gafur <jamilggafur@gmail.com>
2022-07-04 09:07:37 -0500

    working reading, error with linespace in chrom_dataset.py

--- Model/chrom_dataset.py
+++ Model/chrom_dataset.py
@@ -7,7 +7,7 @@ import torch
 from tqdm import tqdm
 import itertools
 import pandas as pd
-
+import pdb
 class Chromatin_Dataset(Dataset):
     def __init__(self, chromType="chr10-chr17", chromName="CTCF-1", file_location="../Data/220627_DATA/TRAIN/*"):
         super(Dataset, self).__init__()
@@ -19,7 +19,7 @@ class Chromatin_Dataset(Dataset):
         return len(self.labels)
         
     def __getitem__(self, index):
-        return torch.tensor(self.data.iloc[index].to_numpy()), torch.tensor(self.labels[index])
+        return torch.tensor(self.data[index], dtype=torch.float32), torch.tensor(self.labels[index],dtype=torch.float32)
 
     def make_onehot(self, sequences, seq_length=32):    
         fd = {  'A' : [1, 0, 0, 0], 'T' : [0, 1, 0, 0], 'G' : [0, 0, 1, 0], 'C': [0, 0, 0, 1], 
@@ -50,16 +50,19 @@ class Chromatin_Dataset(Dataset):
     def readfiles(self,chromeType,chromName, file_location):
         files = glob(file_location)
         labels = []
+        data = []
 
         for i in files:
             if chromeType in i and chromName in i:
                 if ".chromtrack" in i:
                     print("Processing: {}".format(i))
                     value = []
+                    line_num = 0
                     with open(i) as openfile:
                         for line in openfile:
-                            value.append([float(i) for i in line.strip().split(" ")])
-                    data = pd.DataFrame(value)
+                            # TODO figure out different line lengths
+                            data.append([float(i) for i in line.strip().split(" ")[:100]])
+                    data = np.array(data)
             if chromeType in i:
                 if ".label" in i:
                     print("Processing: {}".format(i))
@@ -67,7 +70,7 @@ class Chromatin_Dataset(Dataset):
                     with open(i) as openfile:
                         for line in openfile:
                             value.append(float(line.strip()))
+                    labels = np.array(value)
 
-                    
-
-        return data, value
+        
+        return data, labels
--- Model/main.py
+++ Model/main.py
@@ -1,79 +1,90 @@
+from wsgiref.validate import validator
 from torch.utils.data import random_split as rnd_splt
 from chrom_dataset import Chromatin_Dataset
 from torch.utils.data import DataLoader
 from model import Chromatin_Network
 import torch 
 from torch import nn
+from tqdm import tqdm 
+import pdb
+
 
 def main():
    # Detect GPU or CPU
    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
    # Load the data
-   chrom_data = Chromatin_Dataset()
-   # Split the data (we are doing a 75/25 split)
-   train_percent = .75
-   train_set, val_set = rnd_splt(chrom_data, [int(chrom_data.length*train_percent), chrom_data.length-int(chrom_data.length*train_percent)])
-   
-   # Convert datasets into DataLoaders
-   train_loader = DataLoader(train_set, batch_size=32,shuffle=True)
-   test_loader = DataLoader(val_set, batch_size=32, shuffle=True)
-   
+   c10_17_ctf_1 = DataLoader(Chromatin_Dataset( chromType="chr10-chr17", chromName="CTCF-1"), batch_size=256,shuffle=True)
+   #c10_17_H3K4me3_1 = DataLoader(Chromatin_Dataset( chromType="chr10-chr17", chromName="H3K4me3-1"), batch_size=32,shuffle=True)
+   # c10_17_p300_1 = DataLoader(Chromatin_Dataset( chromType="chr10-chr17", chromName="p300-1"), batch_size=32,shuffle=True)
+
+   trainer = [c10_17_ctf_1]
+   #validator = [c10_17_H3K4me3_1] 
+
+
    # Build the model 
-   model = Chromatin_Network(input_shape=chrom_data.input_shape)
+   model = Chromatin_Network(input_shape=100)
    print(model)
    model = model.to(device)
 
    # Compile the model
-   learning_rate = 0.01 
+   learning_rate = 1e-5
    epochs = 30
    optimizer = torch.optim.SGD(model.parameters(),lr=learning_rate)
    loss_fn = nn.BCELoss()
 
    # Train the model
-   for epoch in range(epochs):
-      train_loss = 0.0
-      # Set model to train mode
-      model.train()
+   for epoch in tqdm(range(epochs), desc="Epochs"):
+      # Set model to train mode and train on training data
+      total_train_loss = 0
+      total_valid_loss = 0
 
-      for data, label in train_loader:
+      for train_loader in trainer:
+         train_loss = 0.0
 
-         # Load data appropriatly
-         data, label = data.to(device), label.to(device)
+         for data, label in train_loader:
+            # Load data appropriatly
+            data, label = data.to(device), label.to(device)
 
-         # Clear gradients
-         optimizer.zero_grad()
+            # Clear gradients
+            optimizer.zero_grad()
 
-         # Forward Pass
-         target = model(data)
+            # Forward Pass
+            target = model(data)
 
-         # Clean the data
-         label = label.unsqueeze(1)
-         
-         # # Calculate the Loss
-         loss = loss_fn(target, label)
+            # Clean the data
+            target = torch.flatten(target)
 
-         # Calculate the gradient
-         loss.backward()
+            # Calculate the Loss
+            loss = loss_fn(target.to(torch.float32), label.to(torch.float32))
 
-         # Update Weight
-         optimizer.step()
+            # Calculate the gradient
+            loss.backward()
 
-         # Calc loss
-         train_loss += loss.item()
+            # Update Weight
+            optimizer.step()
 
-      # Set model to validation 
-      model.eval()
-      valid_loss = 0
-      for valid_data, valid_label in test_loader:
-         valid_data , valid_label = valid_data.to(device) , valid_label.to(device)
+            # Calc loss
+            train_loss += loss.item()
+         total_train_loss += train_loss
+      total_train_loss/=len(trainer)
 
-         target = model(valid_data)
-         valid_label = valid_label.unsqueeze(1)
 
-         loss = loss_fn(target, valid_label)
-         valid_loss += loss.item() 
+      for test_loader in validator:
+          # Set model to validation 
+         model.eval()
+         valid_loss = 0
+         for valid_data, valid_label in test_loader:
+            valid_data , valid_label = valid_data.to(device) , valid_label.to(device)
 
-      print(f'Epoch {epoch+1} \t\t Training Loss: {train_loss / len(train_loader)} \t\t Validation Loss: {valid_loss / len(test_loader)}')
+            target = model(valid_data)
+            valid_label = valid_label.unsqueeze(1)
+
+            loss = loss_fn(target.to(torch.float32), valid_label.to(torch.float32))
+            valid_loss += loss.item() 
+         total_valid_loss+=valid_loss
+      total_valid_loss /= len(validator)
+
+      print(f'Epoch {epoch+1} \t\t Training Loss: {total_train_loss} \t\t Validation Loss: {total_valid_loss}')
 
 main()
