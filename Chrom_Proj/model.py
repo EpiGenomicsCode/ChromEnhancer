21b58dfad14679e4e9d94f0d22f350e4cec0436c
jamil gafur <jamilggafur@gmail.com>
2022-09-22 14:21:47 -0500

    working LSTM

--- Chrom_Proj/model.py
+++ Chrom_Proj/model.py
@@ -1,6 +1,9 @@
 import torch
 from torch import nn
 import torch.nn.functional as F
+import pdb
+from torch.autograd import Variable 
+
 
 """
 Different Neural Networks
@@ -10,11 +13,11 @@ class Chromatin_Network1(nn.Module):
     def __init__(self, name):
         super(Chromatin_Network1,self).__init__()
         self.name = name
-        self.layer_1 = nn.Conv1d(1, 3, 2) 
-        self.layer_2 = nn.Conv1d(3, 5, 2) 
-        self.layer_3 = nn.Conv1d(5, 10, 2)
+        self.layer_1 = nn.Conv1d(1, 3, 10) 
+        self.layer_2 = nn.Conv1d(3, 5, 50) 
+        self.layer_3 = nn.Conv1d(5, 10, 100)
 
-        self.layer_4 = nn.Linear(4970, 256)
+        self.layer_4 = nn.Linear(3430, 256)
         self.layer_5 = nn.Linear(256,128)
         self.layer_out = nn.Linear(128,1)
 
@@ -69,34 +72,42 @@ class Chromatin_Network2(nn.Module):
 
 # LSTM
 class Chromatin_Network3(nn.Module):
-    def __init__(self, name, input_size=1, hidden_size=30, num_classes=1, num_layers=3):
+    def __init__(self, name, input_size=1, hidden_size=30, num_layers=3):
         super(Chromatin_Network3, self).__init__()
         self.name = name
         self.num_layers = num_layers
         self.hidden_size = hidden_size
 
-        self.mem = nn.LSTM(input_size, hidden_size, num_layers,batch_first=True, dropout=.02)
+
+        self.lstm = nn.LSTM(input_size=500, hidden_size=hidden_size,
+                          num_layers=num_layers, batch_first=True) #lstm
 
         self.lin1 = nn.Linear(hidden_size,25)
         self.lin2 = nn.Linear(25,10)
         self.lin3 = nn.Linear(10,5)
         self.lin4 = nn.Linear(5,1)
-        self.h0 = None
-        self.c0 = None
+
+        self.h_0 = None
+        self.c_0 = None
+        self.hidden = None
 
     def forward(self, x):
-        x = x.reshape(-1, 500, 1)
 
-        self.h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
-        self.c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size)
+        if self.h_0 == None:    
+            h_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(x.device) #hidden state
+            c_0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_size)).to(x.device) #internal state
+            self.hidden = (h_0, c_0)
 
-        # out -> (batch, seq, hidden)
-        out, _ = self.mem(x, (self.h0,self.c0))
-        out = out[:,-1,:]
+        x = x.reshape(-1,1,500)
 
+        output, self.hidden = self.lstm(x, self.hidden) #lstm with input, hidden, and internal state
+
+        out = output.reshape(-1,self.hidden_size)
         out = F.relu(self.lin1(out))
         out = F.relu(self.lin2(out))
         out = F.relu(self.lin3(out))
         out = torch.sigmoid(self.lin4(out))
 
+
         return out
+
