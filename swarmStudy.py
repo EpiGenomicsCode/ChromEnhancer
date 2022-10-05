c59117c730e0a8219c0c1d0716307211bdcc2cbb
jamil gafur <jamilggafur@gmail.com>
2022-10-05 12:28:15 -0500

    updated docs

--- swarmStudy.py
+++ swarmStudy.py
@@ -10,49 +10,96 @@ from sklearn.cluster import AgglomerativeClustering
 from Chrom_Proj.visualizer import plotCluster
 
 def main():
-    n_clusters = 5
+    """
+    performs a swarm study on each of the models input to extract features 
+    from the overall network
+
+    Variables:
+        numClusters: the amount of clusters to use for all particles
+        numParticles: the number of particles to use in the test
+    """
+    numClusters = 5
     numParticles = 100
+
+    # Grab all the weights and biases saved
     files = sorted(glob.glob("./output/model_weight_bias/*epoch_10*pt"))
     for f in files:
         print("Processing: {}".format(f.split("/")[-1]))
+        # performs the study 
         s, model = swarmModel(modelLocation=f, modelType=int(f[-4]),numParticles=numParticles
                                 ,gravity=0,epochs=10)
+        # save the particles WRT their model
         saveOutput(s, model,  f[:-3]+"_Swarm.csv")
-        plotData = clusterSwarm(s, n_clusters)
+
+        # Cluster the swarm into different sections
+        plotData = clusterSwarm(s, numClusters)
         print("saving clusters to {}".format(f[:-3]))
+
+        # Save the plots and generate a heatmap of the clusters
         plotCluster(plotData, "output/cluster/{}".format(f[f.rindex("/")+1:-3]), numParticles)
+        
+        # Clean up
         del model
         gc.collect()
 
 def swarmModel(modelLocation="./output/model_weight_bias/model_id_A549_TTV_chr10-chr17_chr10_chr17_epoch_10_BS_32_FL_-Data-220802_DATA_MT_1.pt"
-            , modelType=1, numParticles=10, gravity=10, epochs=10):
+                ,modelType=1
+                ,numParticles=10,
+                gravity=10,
+                epochs=10):
+    """
+    Perform a GSA algorithm on the input to extract features that guarentee an enhancer 
+
+    Input:
+        modelLocation: relative path of where the model weight and bias are saved
+        modelType: the model architecture (see model in Chrom folder)
+        numParticles: the number of particles in the swarm study
+        gravity: the gravitational pull for communication in the GSA swarm
+        epochs: number of epochs in the swarm study
+    """
+    # load the model
     model = loadModel(modelFileName=modelLocation, modelType=modelType)
+    
+    # we do not need the gradients just evaluation
     model.eval()
+
+    # Generate a Swarm
     s = swarm.swarm(numParticles, gravity,  epochs, model)
+    # Run the swarm
     s.run()
+    
+    # return the swarm and associated model
     return s, model
 
-def saveOutput(swarm, model, fileName="swarmOutput.csv"):
+def saveOutput(swarm, 
+                model, 
+                saveLocation="./output/swarm/",
+                fileName="swarmOutput.csv"):
+    """
+    Given an optimized swarm and corosponding model save the position of the swarm 
+    """
     s = swarm
     data = []
+    # get the final position of the swarm
     for particle in s.swarm:
         history = particle.history[-1].tolist()
         history[0].append(model(particle.history[-1]).item())
         data.append(history[0])
 
-
+    # Save the data as a CSV rounded to 4th decimal
     data = pd.DataFrame(data)
     data = data.round(4)
-    data.to_csv("./output/swarm/"+fileName[fileName.rfind("/")+1:], index=False, header=False)
+    data.to_csv(saveLocation+fileName[fileName.rfind("/")+1:], index=False, header=False)
+    
+def clusterSwarm(swarm, numClusters):
+    """ 
+    cluster the particles in the swarm
 
-    plt.clf()
-    for index,row in data.iterrows():
-        plt.plot(list(row))
-    plt.savefig("./output/swarm/"+fileName[fileName.rfind("/")+1:-3]+".png")
-
-
-def clusterSwarm(swarm, n_clusters):
-    hc = AgglomerativeClustering(n_clusters=n_clusters, linkage="ward")
+    input:
+        swarm: swarm object that has ran on a model
+        numCluster: the number of clusters you want to use for the swarm
+    """
+    hc = AgglomerativeClustering(numClusters=numClusters, linkage="ward")
     positions = np.array([birb.position.tolist() for birb in swarm.swarm])
     positions = np.squeeze(positions, axis=1)
     hc = hc.fit(positions)
@@ -65,11 +112,6 @@ def clusterSwarm(swarm, n_clusters):
             plotData[comb[1]].append(comb[0])
 
     return plotData
-
-
-
-
-
 
 
 main()
