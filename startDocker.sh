73dd9adc65ee7b43390ec88346900f3c231c7397
jamil gafur <jamilggafur@gmail.com>
2022-12-22 14:23:42 +0000

    added nohups command to run in background

--- startDocker.sh
+++ startDocker.sh
@@ -1,1 +1,1 @@
- docker run -it --rm --gpus all --name cge -v $PWD:/work cornell_genetics
+nohups docker run --rm --gpus all --name cge -v $PWD:/work cornell_genetics /bin/sh -c 'cd /work; sudo rm -rf output; python runHomoModels.py' &
