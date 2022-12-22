774ce57abdfcbc318e91791a9a1d0f4dba8605c8
jamil gafur <jamilggafur@gmail.com>
2022-12-22 14:29:37 +0000

    proper syntax

--- startDocker.sh
+++ startDocker.sh
@@ -1,1 +1,1 @@
-nohups docker run --rm --gpus all --name cge -v $PWD:/work cornell_genetics /bin/sh -c 'cd /work; sudo rm -rf output; python runHomoModels.py' &
+nohup docker run --rm --gpus all --name cge -v $PWD:/work cornell_genetics /bin/sh -c 'cd /work; sudo rm -rf output; python runHomoModels.py' &
