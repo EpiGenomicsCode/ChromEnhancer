6cd0ccf542b4d47121c3d14a4aab0da731e84409
jamil gafur <jamilggafur@gmail.com>
2022-12-22 16:16:16 +0000

    sudo not needed

--- startDocker.sh
+++ startDocker.sh
@@ -1,1 +1,1 @@
-nohup docker run --rm --gpus all --name cge -v $PWD:/work cornell_genetics /bin/sh -c 'cd /work; sudo rm -rf output; python runHomoModels.py' &
+nohup docker run --rm --gpus all --name cge -v $PWD:/work cornell_genetics /bin/sh -c 'cd /work;  rm -rf output; python runHomoModels.py' &
