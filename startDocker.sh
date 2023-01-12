6d0605ff3e99e9bb7ba787a3b64114e232d2c006
jamil gafur <jamilggafur@gmail.com>
2023-01-12 06:54:20 -0600

    correct tar

--- startDocker.sh
+++ startDocker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohups docker run --rm --gpus all --name cge -e "HOSTNAME=$(cat /etc/hostname)" -v $PWD:/work cornell_genetics /bin/sh -c  "cd /work; rm -rf output; rm *tar; python runHomoModels.py; tar -czvf $(hostname).tar.gz output/" &
+nohup docker run --rm --gpus all --name cge -e "HOSTNAME=$(cat /etc/hostname)" -v $PWD:/work cornell_genetics /bin/sh -c  "cd /work; rm -rf output; rm *tar; python runHomoModels.py; tar -czvf $(hostname).tar.gz output/" &
