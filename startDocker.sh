81a0377e1a80fce45a696334f3a506a348033cfb
jamil gafur <jamilggafur@gmail.com>
2023-01-11 20:35:07 -0600

    added compression to startdocker

--- startDocker.sh
+++ startDocker.sh
@@ -1,1 +1,4 @@
-nohup docker run --rm --gpus all --name cge -v $PWD:/work cornell_genetics /bin/sh -c 'cd /work;  rm -rf output; python runHomoModels.py' &
+#!/bin/bash
+
+OUT=$(hostname).tar.gz
+nohup docker run --rm --gpus all --name cge -v $PWD:/work cornell_genetics /bin/sh -c 'cd /work;  rm -rf output; python runHomoModels.py; tar -czvf $OUT output' & 
