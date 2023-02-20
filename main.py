ee6a4002e0ad0a7cd363c126cac2d7f27472ddbd
jamil gafur <jamilggafur@gmail.com>
2023-02-20 10:23:09 -0600

    logging

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from util.utils import *
 from torch.utils.data import DataLoader
 from util.ad_Swarm import swarm as Swarm
 import glob
+import re
+
 
 def seedEverything(seed=42):
     """
@@ -17,49 +19,79 @@ def seedEverything(seed=42):
     torch.backends.cudnn.deterministic = True
 
 def main():
-    seedEverything()
-    # sequenceStudy()
-    paramatersStudy()
 
-def sequenceStudy():
+    hostname =  os.environ.get("HOSTNAME")
+    ids = getids(hostname)
+    index = getIndex(hostname)
+    epochs = 20
+    batch_size = 64
+    seedEverything()
+    sequenceStudy(epochs)
+    paramatersStudy(ids, index, epochs, batch_size)
+
+def sequenceStudy(epochs=20, batch_size=64):
     trainFiles = glob.glob("Data/230124_CHR-Data_Sequence/CHR-CHROM/TRAIN/*.seq")
-    epochs = 1
     for trainFile in trainFiles:
         name = trainFile[trainFile.rfind("/")+1:-4]
 
         trainData = SeqDS.Sequence_Dataset(trainFile, type="train")
-        trainLoader = DataLoader(trainData, batch_size=64, shuffle=True)
+        trainLoader = DataLoader(trainData, batch_size=batch_size, shuffle=True)
         
         testData = SeqDS.Sequence_Dataset(trainFile, type="test")
-        testLoader = DataLoader(testData, batch_size=64, shuffle=True)
+        testLoader = DataLoader(testData, batch_size=batch_size, shuffle=True)
 
         validData = SeqDS.Sequence_Dataset(trainFile, type="valid")
-        validLoader = DataLoader(validData, batch_size=64, shuffle=True)
+        validLoader = DataLoader(validData, batch_size=batch_size, shuffle=True)
+        # only running on the last model
+
+        model = loadModel(6, name, input_size=4000)
+        print(model)
+        model = runHomoModel(model, trainLoader, testLoader, validLoader, epochs)
+        # run the swarm study
+        swarmStudy(model, name, epochs=10, num_particles=10, gravity=.5, size=4000)
+                    
+        # clear the memory
+        clearCache()
         
-        for i in range(1,7):
-            model = loadModel(i, name, input_size=4000)
-            print(model)
-            model = runHomoModel(model, trainLoader, testLoader, validLoader, epochs)
-            # run the swarm study
-            # swarmStudy(model, name, epochs=10, num_particles=10, gravity=.5)
-                        
-            # clear the memory
-            clearCache()
-        break
-        
+def getids(hostname):
+    """
+        Gets the ids for the study
+    """
+    if "A549" in hostname:
+        ids = ["A549"]
+    elif "MCF7" in hostname:
+        ids = ["MCF7"]
+    elif "HepG2" in hostname:
+        ids = ["HepG2"]
+    elif "K562" in hostname:
+        ids = ["K562"]
+    else:
+        ids =  ["A549" ,"MCF7", "HepG2", "K562"]
+    return ids
        
-def paramatersStudy():
+def getIndex(hostname):
+    """
+        Gets the index for the study
+    """
+    if "-1" in hostname:
+        index = ["-1"]
+    elif "-2" in hostname:
+        index = ["-2"]
+    else:
+        index = ["-1", "-2"]
+    return index
+
+def paramatersStudy(ids, index, epochs=3, batch_size=64):
     """
         Generates the parameters for the study and runs the study
     """
-    ids =  ["A549" ,"MCF7", "HepG2", "K562"]
+    
+
     chromtypes = ["CTCF", "H3K4me3", "H3K27ac", "p300", "PolII"]
     studys = ["chr10-chr17", "chr11-chr7", "chr12-chr8", "chr13-chr9", "chr15-chr16"]
 
-    epochs = 3
-    batch_size = 64
     for id in ids:
-        for indexType in ["-2"]: 
+        for indexType in index: 
             # go through each study
             for study in studys:
                 data1, data2 = study.split("-")
@@ -101,8 +133,8 @@ def paramatersStudy():
                         clearCache()
 
 
-def swarmStudy(model, name, epochs=10, num_particles=10, gravity=.5):
-    swarm = Swarm.swarm(num_particles, gravity,  epochs, model)
+def swarmStudy(model, name, epochs=10, num_particles=10, gravity=.5, size=500):
+    swarm = Swarm.swarm(num_particles, gravity,  epochs, model, size=size)
     swarm.run()
     # get all particles in swarm
     particles = np.array([i.position.numpy() for i in swarm.swarm])
@@ -114,8 +146,6 @@ def swarmStudy(model, name, epochs=10, num_particles=10, gravity=.5):
     # plot the clusters
     plotClusters(cluster, particles, name)
 
-
-
-                        
+                       
 if __name__ == "__main__":
     main()
