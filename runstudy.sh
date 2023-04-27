f742b7382e6e1362a6e06ab2e8af656f531477f8
jamil gafur <jamilggafur@gmail.com>
2023-04-27 15:36:34 +0000

    updated runner

--- runstudy.sh
+++ runstudy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rm *.out
-nohup docker run --rm --gpus all --name cge -e "HOSTNAME=$(cat /etc/hostname)" -v $PWD:/work cornell_genetics /bin/sh -c  "cd /work; rm -rf output; rm *tar;  python main.py --???; tar -czvf $(hostname).tar.gz output/" &
+# $1: study name
+rm -rf output/ *out; pkill -9 python; clear; nohup python main.py --$1 &  
