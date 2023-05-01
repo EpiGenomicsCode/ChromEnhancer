0bd1587aab133dc4ed4def1fc6b6b0bf8481c0dd
jamil gafur <jamilggafur@gmail.com>
2023-05-01 03:52:38 +0000

    updated parameter study

--- main.py
+++ main.py
@@ -86,8 +86,9 @@ def paramatersStudy(cellLine, index, epochs=3, batch_size=64, bin_size=1024):
     chromatine =  ["CTCF", "H3K4me3", "H3K27ac", "p300", "PolII"]
     studys = ["chr10-chr17", "chr11-chr7", "chr12-chr8", "chr13-chr9", "chr15-chr16"]
     fileLocation = "./Data/220802_DATA/"
+    
     params = []
-    for id in cellLine:
+    for cellLine in cellLines:
         for types in ["-1", "-2"]: 
             # go through each study
             for study in studys:
@@ -96,14 +97,21 @@ def paramatersStudy(cellLine, index, epochs=3, batch_size=64, bin_size=1024):
                 for data  in [ [data1, data2], [data2, data1]]:
                     test = data[0]
                     valid = data[1]
-                    
-                    cldrop = [i for i in cellLines if i != id]
-                    chdrop = []
                     for modelType in args.model:
-                        # drop all celllines except the one we are using
-                        name = f"Param_study_{study}_test_{test}_valid_{valid}_model{modelType}_clkeep_{'-'.join([i for i in cellLines if i not in cldrop])}_chkeep_{'-'.join([i for i in chromatine if i not in chdrop])}_type{types}"
-                        if name not in params:
-                            params.append([study, test, valid,chdrop, cldrop, types, name, epochs, batch_size, bin_size, modelType, fileLocation])
+                        name = f"study_{study}_test_{test}_valid_{valid}_model{modelType}_clkeep_{cellLine}_chkeep_{'-'.join(chromatine)}_type{types}"
+                        params.append( [
+                            study, # chr10-chr17
+                            test, # chr10
+                            valid, # chr17
+                            chromatine, # chUse
+                            [cellLine], # clUse: ["A549"]
+                            types, # -1
+                            name, # study_chr10-chr17_test_chr10_valid_chr17_model1_clkeep_A549_chkeep_CTCF-H3K4me3-H3K27ac-p300-PolII_type-1
+                            epochs, # 20
+                            batch_size, # 2048  
+                            modelType, # 1
+                            fileLocation # ./Data/220802_DATA/                           
+                        ] )
     
     parseParam("param.log", params)
                         
@@ -170,8 +178,6 @@ def ChromatineDropout(cellLine, index, epochs=3, batch_size=64, bin_size=1024):
     parseParam("CHD.log", params)
 
 
-
-# Function to check if a simulation has already been started
 def simulation_started(started_file, simulation_name):
     sim =  open(started_file, "r")
     for line in sim:
@@ -186,48 +192,32 @@ def mark_simulation_as_started(started_file,simulation_name):
         f.write(simulation_name + "\n")
 
 
-def parseParam(startedFile, params):
-    params.sort(key=lambda x: x[6])
-    # create a new file if it does not exist
-    if not os.path.exists(startedFile):
-        with open(startedFile, "w") as f:
-            f.write("")
-    # print the params  
 
+def parseParam(startedFile, params):
+    # print the params  
     for i in params:
         print(i[6])
 
     for i in tqdm.tqdm(params):
-        study = i[0]
-        test = i[1]
-        valid = i[2]
-        chrdrop = i[3]
-        cldrop = i[4]
-        types = i[5]
         name = i[6]
-        epochs = i[7]
-        batch_size = i[8]
-        bin_size = i[9]
-        modelconfig = i[10]
-        fileLocation = i[11]
-        clearCache()
         if simulation_started(startedFile, name):
             print(f"{name} has already been started, skipping.")
             continue
         else:
             mark_simulation_as_started(startedFile, name)
-            runStudy(study, test, valid, chrdrop, cldrop, types, name, epochs, batch_size, bin_size, modelconfig, fileLocation)    
-
-def runStudy(study, test, valid, chrdrop, cldrop, types, name, epochs, batch_size, bin_size, modelconfig, fileLocation):
-    clearCache()
+            runStudy(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7], i[8], i[9], i[10])
+        
+def runStudy(study, test, valid, chrUse, clUse, types, name, epochs, batch_size,modelconfig, fileLocation):
+  
     ds_train, ds_test, ds_valid = DS.getData(   trainLabel=study,
                                                 testLabel=test,
                                                 validLabel=valid,
-                                                chrDrop=chrdrop,
-                                                cellLineDrop=cldrop,
-                                                bin_size=bin_size,
-                                                fileLocation=fileLocation, 
-                                                dataTypes =types)
+                                                dataTypes=types,
+                                                fileLocation=fileLocation,
+                                                chrUse=chrUse,
+                                                cellLineUse=clUse
+                                            )
+    
     # cast each dataset to a pytorch dataloader
     train_loader = DataLoader(ds_train, batch_size=batch_size )
     test_loader = DataLoader(ds_test, batch_size=batch_size )
