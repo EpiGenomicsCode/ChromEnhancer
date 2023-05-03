54a4ef0ed9fcaa92c8b36100cc870078e7fa1923
jamil gafur <jamilggafur@gmail.com>
2023-05-03 19:04:08 +0000

    updated input size to account for large dataset

--- util/models/ChrNet2.py
+++ util/models/ChrNet2.py
@@ -33,7 +33,7 @@ class Chromatin_Network2(nn.Module):
             self.inputDNN = 6400
         
         if self.input_size == 32900:
-            self.inputDNN = 526336
+            self.inputDNN = 526400
 
         # Define the fully-connected layers
         self.dnn = nn.Sequential(
